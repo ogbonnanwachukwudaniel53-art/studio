eec0de49f1e86b27e6a3d3a223574e47bdf5ab03
ogbonna nwachukwu daniel <ogbonnanwachukwudaniel53@gmail.com>
2025-09-18 22:57:58 +0000

    make sure app uses HTTPS, Does not redirect HTTP traffic to HTTPS ,Make

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,48 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
+  },
+  async headers() {
+    const securityHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://placehold.co https://images.unsplash.com https://picsum.photos https://i.pravatar.cc; font-src 'self' https://fonts.gstatic.com; object-src 'none'; frame-ancestors 'none'; require-trusted-types-for 'script';",
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+      },
+    ];
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
