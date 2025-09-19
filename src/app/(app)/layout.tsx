9cf4bd162d81bd13d3e3107b198bfe1a9e1587f8
ogbonna nwachukwu daniel <ogbonnanwachukwudaniel53@gmail.com>
2025-09-19 22:21:39 +0000

    remove layout (_for element <DropdownMenuLabel>_)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -211,7 +211,7 @@ function MainAppLayout({ children }: { children: React.ReactNode }) {
                               {getInitials(currentUser.name)}
                           </AvatarFallback>
                       </Avatar>
-                      <span>{currentUser.name}</span>
+                      <span className="hidden md:inline">{currentUser.name}</span>
                       <ChevronDown className="h-4 w-4" />
                   </Button>
               </DropdownMenuTrigger>
